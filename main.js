6953cc1fda89e2e3916ec6a792a9d34db09ba15e
prabhav07022006 <prabhav722006@gmail.com>
2025-02-18 15:56:38 +0530

    lol

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+function validateAndRedirect(){
+    let name = document.getElementById("name_1").value.trim();
+    let email = document.getElementById("email_1").value.trim();
+    let password = document.getElementById("password_1").value.trim();
+    let errorMessage = document.getElementById("errorMessage");
 
+    if(name ===""||email === "" || password === ""){
+        errorMessage.textContent="Please fill all fields"
+    }else{
+        errorMessage.textContent="";
+        window.location.href="index_1.html";
+    }
+
+}
