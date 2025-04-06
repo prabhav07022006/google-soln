afea750511e90385b4cea36305dfed39338f7fe7
Rom <romitrajbind0@gmail.com>
2025-04-06 12:56:50 +0530

    pern

--- MainServer.js
+++ MainServer.js
@@ -1,12 +1,13 @@
 const express  = require('express');
 const app      = express();
 const path     = require('path');
-const mongoose = require('mongoose');
 const user     = require('./UserSchema');
 const { type } = require('os');
 const cors     = require('cors');
 const axios    = require('axios');
 const fuzz     = require('fuzzball');
+const pool		 = require('pg');
+
 
 const allowedQuestions = [
     "What kind of diet should I follow?",
@@ -126,31 +127,31 @@ app.get('/api/find', async (req, res)=>{
 })
 
 app.post('/chat', async (req, res) => {
-    const userMessage = req.body.message;
-    const match = fuzz.extract(userMessage, allowedQuestions, { scorer: fuzz.token_set_ratio, returnObjects: true })[0];
-    if (match.score < 80) {
-      return res.json({ reply: "Sorry, I can only answer specific questions." });
-    }
-    try {
-      const response = await axios.post(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
-        {
-          contents: [
-            {
-              parts: [{ text: userMessage +"Please follow these rules while answering: 1.Please answer concisely 2. Do not provide irrelevant information.3. Answer in simple, easy-to-understand language.5. Answer in a kind and soft tone.6. The answer should not exceed 60 words.7.Do not answer any suspicious questions" }]
-            }
-          ]
-        }
-      );
-  
-      const reply = response.data.candidates[0].content.parts[0].text;
-      res.json({ reply });
-    } catch (error) {
-      console.error('API error:', error.response?.data || error.message);
-      res.status(500).json({ error: 'Failed to fetch from Gemini API' });
-    }
-  });
-  
+  const userMessage = req.body.message;
+  const match = fuzz.extract(userMessage, allowedQuestions, { scorer: fuzz.token_set_ratio, returnObjects: true })[0];
+  if (match.score < 80) {
+    return res.json({ reply: "Sorry, I can only answer specific questions." });
+  }
+  try {
+    const response = await axios.post(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      {
+				contents: [
+          {
+            parts: [{ text: userMessage +"Please follow these rules while answering: 1.Please answer concisely 2. Do not provide irrelevant information.3. Answer in simple, easy-to-understand language.5. Answer in a kind and soft tone.6. The answer should not exceed 60 words.7.Do not answer any suspicious questions" }]
+          }
+        ]
+      }
+    );
+    const reply = response.data.candidates[0].content.parts[0].text;
+    res.json({ reply });
+  } catch (error) {
+    console.error('API error:', error.response?.data || error.message);
+    res.status(500).json({ error: 'Failed to fetch from Gemini API' });
+  }
+});
+
+
 app.listen(4000, ()=>{
 	console.log("server is running");
 })
